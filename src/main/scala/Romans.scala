916d1d2b5e28ff699ce58ee753f3b13fe9e22483
Julian Schrenk <kjs@fastmail.com>
2022-03-18 23:05:07 +0000

    Pass 18.

--- src/main/scala/Romans.scala
+++ src/main/scala/Romans.scala
@@ -2,6 +2,9 @@ package com.kjs73.romans
 
 object Romans extends App {
   def roman(arabic: Int): String = {
+    if (arabic >= 10) {
+      return "X" + roman(arabic - 10)
+    }
     if (arabic == 9) {
       return "IX"
     }
--- src/test/scala/RomansTest.scala
+++ src/test/scala/RomansTest.scala
@@ -6,7 +6,8 @@ import scala.language.postfixOps
 
 class RomansTest extends FunSuite {
   test("Romans.roman") {
-    val roman: List[String] = List("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX")
+    val roman: List[String] = List("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII",
+      "XIV", "XV", "XVI", "XVII", "XVIII")
     val arabic: List[Int] = 1 to roman.length toList;
     for((a,r) <- arabic zip roman) {
       assert(Romans.roman(a) === r)
