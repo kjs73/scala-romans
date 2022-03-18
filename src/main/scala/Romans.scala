048357bafc760a957a3b1eab8c8e4f4ef616b394
Julian Schrenk <kjs@fastmail.com>
2022-03-18 22:57:39 +0000

    Pass 6.

--- src/main/scala/Romans.scala
+++ src/main/scala/Romans.scala
@@ -2,8 +2,8 @@ package com.kjs73.romans
 
 object Romans extends App {
   def roman(arabic: Int): String = {
-    if (arabic == 5) {
-      return "V";
+    if (arabic >= 5) {
+      return "V" + roman(arabic - 5)
     }
     if (arabic == 4) {
       return "IV"
